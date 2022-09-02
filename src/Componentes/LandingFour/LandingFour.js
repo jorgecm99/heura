1cee752a452b8c1b811ce9f909c4e64fa4ab04bb
Mathew <mathew@MH.local>
2022-09-02 16:13:05 +0200

    last changes2

--- src/Componentes/LandingFour/LandingFour.js
+++ src/Componentes/LandingFour/LandingFour.js
@@ -137,7 +137,9 @@ const LandingFour = () => {
         <div>
             <div className='landing-two-div-four-container footer' data-aos="fade-up" data-aos-duration="2000">
             <div className='landing-two-div-four-title landing-two-h2-new-underline footer-text'>Want to <br></br> know more?</div>        
-            <button className='btn-landing'><a target="_blank" rel="noopener noreferrer" href='https://bstadium.es/quienes-somos/'>Contact us</a></button>
+            <button className='btn-landing'><a target="_blank" rel="noreferrer" href="mailto:hola@bstadium.es">Contact us</a></button>
+
+            
         </div>
     </div>
 
