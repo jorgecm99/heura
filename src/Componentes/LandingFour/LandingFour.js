1963cbb1cb7d107cc5fffa9136207eabc4956519
Mathew <mathew@MH.local>
2022-09-02 16:07:22 +0200

    more changes

--- src/Componentes/LandingFour/LandingFour.js
+++ src/Componentes/LandingFour/LandingFour.js
@@ -9,6 +9,12 @@ AOS.init();
 
 const LandingFour = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isMenuOpen2, setIsMenuOpen2] = useState(false);
+    const [isMenuOpen3, setIsMenuOpen3] = useState(false);
+    const [isMenuOpen4, setIsMenuOpen4] = useState(false);
+    const [isMenuOpen5, setIsMenuOpen5] = useState(false);
+    /* const [isMenuOpen6, setIsMenuOpen6] = useState(false);
+    const [isMenuOpen7, setIsMenuOpen7] = useState(false); */
 
   return (
     <div className='landing-four'>
@@ -17,7 +23,7 @@ const LandingFour = () => {
         </div>
         <div className='landing-four-dropdown'>
             <div className='landing-four-dropdown-main' data-aos="fade-up" data-aos-duration="2000">
-            <h4 className='landing-four-dropdown-title'>¿cuándo se lanzará la campaña?</h4>
+            <h4 className='landing-four-dropdown-title'>When will the campaign go live?</h4>
             <div className='landing-four-dropdown-icon'>
                         {
                             isMenuOpen ?
@@ -31,10 +37,98 @@ const LandingFour = () => {
                             isMenuOpen ? 
                                 <div className='landing-four-dropdown-content'>
                                     <p className='landing-four-dropdown-content-text'>
-                                    Se lanzará al público general en abril de 2022. Para nuestros amigos 
-                                    registrados previamente, tendrán acceso exclusivo para invertir en la 
-                                    plataforma 48 horas antes de la fecha oficial. Os avisaremos de la fecha 
-                                    exacta con antelación ;-)
+                                    It’s going live to the wider public in April 2022. For our pre-registered friends, you will have exclusive access to invest on the platform 48hrs prior to this. We’ll let you know the exact date in advance ;-)
+                                    </p>
+                    </div> : null
+                }
+        </div>
+
+        <div className='landing-four-dropdown'>
+            <div className='landing-four-dropdown-main' data-aos="fade-up" data-aos-duration="2000">
+            <h4 className='landing-four-dropdown-title'>Who can invest?</h4>
+            <div className='landing-four-dropdown-icon'>
+                        {
+                            isMenuOpen2 ?
+                                 <FontAwesomeIcon icon={faX} style={{color:"white"}} onClick={()=>setIsMenuOpen2(false)}/>
+                                : <FontAwesomeIcon icon={faPlus} style={{color:"white"}} onClick={()=>setIsMenuOpen2(true)}/>
+                        }
+                        </div>
+                        </div>
+
+                        {
+                            isMenuOpen2 ? 
+                                <div className='landing-four-dropdown-content'>
+                                    <p className='landing-four-dropdown-content-text'>
+                                    You must be over 18 years old and legally entitled to invest.
+                                    </p>
+                    </div> : null
+                }
+        </div>
+
+        <div className='landing-four-dropdown'>
+            <div className='landing-four-dropdown-main' data-aos="fade-up" data-aos-duration="2000">
+            <h4 className='landing-four-dropdown-title'>Can you explain your Ebitda?</h4>
+            <div className='landing-four-dropdown-icon'>
+                        {
+                            isMenuOpen3 ?
+                                 <FontAwesomeIcon icon={faX} style={{color:"white"}} onClick={()=>setIsMenuOpen3(false)}/>
+                                : <FontAwesomeIcon icon={faPlus} style={{color:"white"}} onClick={()=>setIsMenuOpen3(true)}/>
+                        }
+                        </div>
+                        </div>
+
+                        {
+                            isMenuOpen3 ? 
+                                <div className='landing-four-dropdown-content'>
+                                    <p className='landing-four-dropdown-content-text'>
+                                    Our EBITDA for FY21 reached €6M negative. This is driven by the fact that we are a company investing in growth and therefore are adding resources first to collect gains in the future, creating a loss in the initial stage.                                    
+                                    </p>
+                                    <p className='landing-four-dropdown-content-text'>                                    
+                                    International expansion is one of the biggest bets. However, our business is still at a very early stage in the majority of the countries so this increases the impact on a negative EBITDA.
+                                    </p>
+                    </div> : null
+                }
+        </div>
+
+        <div className='landing-four-dropdown'>
+            <div className='landing-four-dropdown-main' data-aos="fade-up" data-aos-duration="2000">
+            <h4 className='landing-four-dropdown-title'>What's next?</h4>
+            <div className='landing-four-dropdown-icon'>
+                        {
+                            isMenuOpen4 ?
+                                 <FontAwesomeIcon icon={faX} style={{color:"white"}} onClick={()=>setIsMenuOpen4(false)}/>
+                                : <FontAwesomeIcon icon={faPlus} style={{color:"white"}} onClick={()=>setIsMenuOpen4(true)}/>
+                        }
+                        </div>
+                        </div>
+
+                        {
+                            isMenuOpen4 ? 
+                                <div className='landing-four-dropdown-content'>
+                                    <p className='landing-four-dropdown-content-text'>
+                                    We will be in touch with you over the next few days with more information on our crowd raise, how to do it and which rights this gives to you. Make sure you’re signed up to Crowdcube early - seriously, don’t wait till the day. Tell all your friends to do the same!
+                                    </p>
+                    </div> : null
+                }
+        </div>
+
+        <div className='landing-four-dropdown'>
+            <div className='landing-four-dropdown-main' data-aos="fade-up" data-aos-duration="2000">
+            <h4 className='landing-four-dropdown-title'>When will they become shares?</h4>
+            <div className='landing-four-dropdown-icon'>
+                        {
+                            isMenuOpen5 ?
+                                 <FontAwesomeIcon icon={faX} style={{color:"white"}} onClick={()=>setIsMenuOpen5(false)}/>
+                                : <FontAwesomeIcon icon={faPlus} style={{color:"white"}} onClick={()=>setIsMenuOpen5(true)}/>
+                        }
+                        </div>
+                        </div>
+
+                        {
+                            isMenuOpen5 ? 
+                                <div className='landing-four-dropdown-content'>
+                                    <p className='landing-four-dropdown-content-text'>
+                                    The invested resources will be converted into shares when we close the future financing round. Crowdcube will automatically contact Heura Foods to ensure that the conversion occurs at the appropriate point and they will send you an email when the actions are issued so that you can download your declaration of beneficial ownership in your Crowdcube profile.
                                     </p>
                     </div> : null
                 }
--- src/Componentes/LandingThree/LandingThree.js
+++ src/Componentes/LandingThree/LandingThree.js
@@ -5,6 +5,13 @@ import './LandingThree.css';
 
 import desafioSevillaPitchmin from '../../Assets/Images/desafioSevillaPitchmin.jpg';
 import granada from '../../Assets/Images/tourgranada.jpg';
+/* import experienciasinteractivas from '../../Assets/Images/experiencias-interactivas-min.jpg'; */
+import toursoriginales from '../../Assets/Images/tours-originales-de-futbol-min.jpg';
+/* import tourinteractivos from '../../Assets/Images/tour-interactivos-por-estadios-y-planes-de-ocio-min.jpg'; */
+import entrenamientos from '../../Assets/Images/enfrentamiento-o-actividades-grupales-min.jpg';
+import seleccion from '../../Assets/Images/entrena-con-la-seleccion-min.jpg';
+import eventos from '../../Assets/Images/eventos-corporativos-min.jpg';
+import entradas from '../../Assets/Images/entradas-exclusivas-y-fan-zones-min.jpg';
 import match from '../../Assets/Images/match-experience.png';
 
 
@@ -19,44 +26,89 @@ const LandingThree = () => {
             
       <div className='landing-three-card-three' data-aos="fade-up" data-aos-duration="2000">
         <h1 className='landing-three-card-one-title'>Business</h1>
-        <h1 className='landing-three-card-two-black'>Highlights</h1>      
-        <p style={{color:"white", lineHeight:2.2}}><strong className='strong-underline'>Tripled revenue</strong> in 2022 reaching +500k€ Q1 & Q2 '22 <strong className='strong-underline'>grew by 125%</strong> in comparison with the previous period.</p>
-        <p style={{color:"white", lineHeight:2.2}}><strong className='strong-underline'> More than</strong> <strong className='strong-underline'>3,000 football experiences sold</strong> 17 clubs in 1 market (Spain)</p>        
+        <h1 className='landing-three-card-two-black'>Highlights</h1>
+        <ul>
+          <li style={{color:"white", lineHeight:2.2, textAlign:'left'}}><strong className='strong-underline'> Tripled revenue</strong> in 2022 reaching +500k€.</li>
+          <li style={{color:"white", lineHeight:2.2, textAlign:'left'}}>Q1 & Q2 '22 <strong className='strong-underline'>grew by 125%</strong> in comparison with the previous period.</li>
+          <li style={{color:"white", lineHeight:2.2, textAlign:'left'}}><strong className='strong-underline'> More than</strong> <strong className='strong-underline'>3,000 football experiences sold</strong> 17 clubs in 1 market (Spain)</li>
+        </ul>             
       </div>
 
       <div className='landing-three-card-one' data-aos="fade-up" data-aos-duration="2000">
-        <h1 className='landing-three-card-one-title'>Products</h1>             
-        <p style={{color:"white", lineHeight:2.2}}>We have launched our spectaculars experiences.</p>
-        
-        <div style={{marginTop:30}}>
-          <div style={{color:"#01FF95", textTransform:"uppercase"}}>Tour Interactivo Los Carmenes</div>
-          <img src={granada} alt='' className='landing-two-div-three-image' style={{width:250,marginTop:20, marginBottom:20, borderRadius:10}}></img>          
-        </div>
+      <h1 className='landing-three-card-one-title'>PRODUCTS</h1>
+      <p style={{color:"white", lineHeight:2.2}}>We have launched our spectaculars experiences.</p>
+                                           
+      </div> 
 
-        <div style={{marginTop:30}}>
-          <div style={{color:"#01FF95", textTransform:"uppercase"}}>MATCH EXPERIENCE</div>
-          <img src={match} alt='' className='landing-two-div-three-image' style={{width:250,marginTop:20, marginBottom:20, borderRadius:10}}></img>          
-        </div>
-        
-      </div>    
+      <div className='landing-three-card-one' data-aos="fade-up" data-aos-duration="2000">
+          <h1 className='landing-three-card-two-title'>New virtual</h1>
+          <h1 className='landing-three-card-two-black'>physical experiences</h1>                              
+          <div style={{marginTop:20}}>          
+            <img src={granada} alt='' className='landing-two-div-three-image' style={{width:250,marginTop:0, marginBottom:20, borderRadius:10}}></img>          
+            <button className='btn-landing'><a target="_blank" rel="noopener noreferrer" href='https://bstadium.es/granada-cf/tour-por-el-nuevo-los-carmenes-estadio-de-granada-cf/'>Discover it!</a></button>
+          </div>               
+      </div> 
 
       <div className='landing-three-card-three' data-aos="fade-up" data-aos-duration="2000">
-          <h1 className='landing-three-card-two-title'>New virtual</h1> 
-          <h1 className='landing-three-card-two-black'>physical experiences</h1>
-          <img src={granada} alt='' className='landing-two-div-three-image' style={{width:250,marginTop:50, marginBottom:20, borderRadius:10}}></img>
+        <h1 className='landing-three-card-two-black'>Exclusive Tickets &</h1>
+        <h1 className='landing-three-card-two-title'>Fan Zones</h1>                               
+          <div style={{marginTop:20}}>          
+            <img src={entradas} alt='' className='landing-two-div-three-image' style={{width:250,marginTop:0, marginBottom:20, borderRadius:10}}></img>          
+            <button className='btn-landing'><a target="_blank" rel="noopener noreferrer" href='https://bstadium.es/entradas-de-partidos/'>Get your tikect!</a></button>
+          </div>               
+      </div> 
+
+      <div className='landing-three-card-one' data-aos="fade-up" data-aos-duration="2000">        
+          <h1 className='landing-three-card-one-title'>Original Tours</h1>                                
+          <div style={{marginTop:20}}>          
+            <img src={toursoriginales} alt='' className='landing-two-div-three-image' style={{width:250,marginTop:0, marginBottom:20, borderRadius:10}}></img>          
+            <button className='btn-landing'><a target="_blank" rel="noopener noreferrer" href='https://bstadium.es/real-madrid-cf/tour-por-el-estadio-santiago-bernabeu-y-ruta-turistica-madrid-y-el-futbol/'>See more!</a></button>
+          </div>               
+      </div> 
+
+      <div className='landing-three-card-three' data-aos="fade-up" data-aos-duration="2000">        
+          <h1 className='landing-three-card-one-black'>Training with </h1>
+          <h1 className='landing-three-card-two-title'>the national team</h1>                                
+          <div style={{marginTop:20}}>          
+            <img src={seleccion} alt='' className='landing-two-div-three-image' style={{width:250,marginTop:0, marginBottom:20, borderRadius:10}}></img>          
+            <button className='btn-landing'><a target="_blank" rel="noopener noreferrer" href='https://bstadium.es/salta-al-campo/entrena-como-la-seleccion-espanola-iniciacion/'>See more!</a></button>
+          </div>               
+      </div> 
+
+      <div className='landing-three-card-one' data-aos="fade-up" data-aos-duration="2000">        
+          <h1 className='landing-three-card-one-title'>Corporate events &</h1>    
+          <h1 className='landing-three-card-one-black'>digital experiences</h1>                            
+          <div style={{marginTop:20}}>          
+            <img src={eventos} alt='' className='landing-two-div-three-image' style={{width:250,marginTop:0, marginBottom:20, borderRadius:10}}></img>          
+            <button className='btn-landing'><a target="_blank" rel="noopener noreferrer" href='https://bstadium.es/real-madrid-cf/tour-por-el-estadio-santiago-bernabeu-y-ruta-turistica-madrid-y-el-futbol/'>See more!</a></button>
+          </div>               
+      </div> 
+         
+
+      <div className='landing-three-card-three' data-aos="fade-up" data-aos-duration="2000">
+          <h1 className='landing-three-card-two-black'>Trainings &</h1> 
+          <h1 className='landing-three-card-two-title'>group activities</h1>
+          <img src={entrenamientos} alt='' className='landing-two-div-three-image' style={{width:250,marginTop:50, marginBottom:20, borderRadius:10}}></img>
+          <button className='btn-landing'><a target="_blank" rel="noopener noreferrer" href='https://bstadium.es/real-madrid-cf/tour-por-el-estadio-santiago-bernabeu-y-ruta-turistica-madrid-y-el-futbol/'>See more!</a></button>
       </div>
 
-      <div className='landing-three-card-one' data-aos="fade-up" data-aos-duration="2000">
-        <h1 className='landing-three-card-one-title'>New</h1>
-        <h1 className='landing-three-card-two-black'>launching!</h1>
-          {/* <p style={{color:"white"}}>We have launched our <span className='strong-underline'>spectaculars experiences:</span></p> */}
+      <div className='landing-three-card-one' data-aos="fade-up" data-aos-duration="2000">        
+          <h1 className='landing-three-card-one-title'>Match Days</h1>
+          
+          <div style={{marginTop:20}}>          
+            <img src={match} alt='' className='landing-two-div-three-image' style={{width:250,marginTop:0, marginBottom:20, borderRadius:10}}></img>          
+            <button className='btn-landing'><a target="_blank" rel="noopener noreferrer" href='https://vimeo.com/677679114'>See more!</a></button>
+          </div>               
+      </div> 
+
+      <div className='landing-three-card-three' data-aos="fade-up" data-aos-duration="2000">
+          <h1 className='landing-three-card-one-title'>New launching!</h1>
+          <h1 className='landing-three-card-two-black'>Challenge Sevilla</h1>
           <img src={desafioSevillaPitchmin} alt='' className='landing-two-div-three-image' style={{width:250,marginTop:30, marginBottom:20}}></img>
-      </div>      
+          <button className='btn-landing'><a target="_blank" rel="noopener noreferrer" href='https://desafioestadiosevilla.com/'>See more!</a></button>
+      </div>
 
-    
-
-        
-      
+                          
 
     </div>
   )
